7a2e67ea98e87f821b2ebebb97471a0a1eff9339
Andrew <aikhramov@edu.hse.ru>
2025-04-09 18:39:28 +0300

    added sorting by article, fixed column widths in the report

--- reports/excel_generator.py
+++ reports/excel_generator.py
@@ -1,7 +1,18 @@
 import os
 import pandas as pd
-
+from openpyxl import load_workbook
 from config import REPORT_FOLDER
+
+def change_width_column(sheet):
+    for col in sheet.iter_cols(1, sheet.max_column):
+        if col[0].value == "Наименование":
+            for cell in col:
+                cell.alignment = cell.alignment.copy(wrap_text=True)
+            sheet.column_dimensions[col[0].column_letter].width = 60
+        else:
+            for cell in col:
+                cell.alignment = cell.alignment.copy(wrap_text=True)
+            sheet.column_dimensions[col[0].column_letter].width = 20
 
 
 def generate_excel(stock_data, movement_data, stock_pay_user_data, semi_finished_products, products):
@@ -22,6 +33,9 @@ def generate_excel(stock_data, movement_data, stock_pay_user_data, semi_finished
         semi_finished_products_df = pd.DataFrame(semi_finished_products)
         products_df = pd.DataFrame(products)
 
+        if "Артикул" in stock_df.columns:
+            stock_df = stock_df.sort_values(by="Артикул")
+
         report_file = os.path.join(REPORT_FOLDER, "warehouse_report.xlsx")
         with pd.ExcelWriter(report_file, engine="openpyxl") as writer:
             stock_df.to_excel(writer, sheet_name="Stock", index=False)
@@ -29,6 +43,16 @@ def generate_excel(stock_data, movement_data, stock_pay_user_data, semi_finished
             stock_pay_user_df.to_excel(writer, sheet_name="Accounts Balances", index=False)
             semi_finished_products_df.to_excel(writer, sheet_name="Semi-Finished Products", index=False)
             products_df.to_excel(writer, sheet_name="Products", index=False)
+
+        sheets = ["Stock", "Movements", "Accounts Balances", "Semi-Finished Products", "Products"]
+
+        wb = load_workbook(report_file)
+
+        for name_sheet in sheets:
+            sheet = wb[name_sheet]
+            change_width_column(sheet)
+
+        wb.save(report_file)
 
         return report_file
     except Exception as e:
